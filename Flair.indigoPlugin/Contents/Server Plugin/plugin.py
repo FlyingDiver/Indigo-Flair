858f7ab8126f8f7ca4ae8e1cd6c2e79b8890b36a
Joe Keenan <joe@flyingdiver.com>
2020-01-04 17:09:20 -0500

    Clean up

--- Flair.indigoPlugin/Contents/Server Plugin/plugin.py
+++ Flair.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -8,41 +8,6 @@ import time
 
 from flair import FlairAccount
 
-import temperature_scale
-
-REFRESH_TOKEN_PLUGIN_PREF='refreshToken-'
-TEMPERATURE_SCALE_PLUGIN_PREF='temperatureScale'
-
-
-TEMP_FORMATTERS = {
-    'F': temperature_scale.Fahrenheit(),
-    'C': temperature_scale.Celsius(),
-    'K': temperature_scale.Kelvin(),
-    'R': temperature_scale.Rankine()
-}
-
-#   Plugin-enforced minimum and maximum setpoint ranges per temperature scale
-ALLOWED_RANGE = {
-    'F': (40,95),
-    'C': (6,35),
-    'K': (277,308),
-    'R': (500,555)
-}
-
-kHvacModeEnumToStrMap = {
-    indigo.kHvacMode.Cool               : u"cool",
-    indigo.kHvacMode.Heat               : u"heat",
-    indigo.kHvacMode.HeatCool           : u"auto",
-    indigo.kHvacMode.Off                : u"off",
-    indigo.kHvacMode.ProgramHeat        : u"program heat",
-    indigo.kHvacMode.ProgramCool        : u"program cool",
-    indigo.kHvacMode.ProgramHeatCool    : u"program auto"
-}
-
-kFanModeEnumToStrMap = {
-    indigo.kFanMode.Auto            : u"auto",
-    indigo.kFanMode.AlwaysOn        : u"on"
-}
 
 class Plugin(indigo.PluginBase):
 
@@ -128,11 +93,6 @@ class Plugin(indigo.PluginBase):
                         else:
                             indigo.devices[accountID].updateStateImageOnServer(indigo.kStateImageSel.SensorTripped)
                             self.logger.debug("Flair account {} not authenticated, skipping update".format(accountID))
-
-                    # now update all the Indigo devices         
-                    
-#                    for dev in self.flair_thermostats.values():
-#                        dev.update()
                                         
 
                 # Refresh the auth tokens as needed.  Refresh interval for each account is calculated during the refresh
@@ -207,11 +167,8 @@ class Plugin(indigo.PluginBase):
 
         
     def deviceStartComm(self, dev):
-
         self.logger.info(u"{}: Starting {} Device {}".format(dev.name, dev.deviceTypeId, dev.id))
         
-        dev.stateListOrDisplayStateIdChanged()
-
         if dev.deviceTypeId == 'FlairAccount':     # create the Flair account object.  It will attempt to refresh the auth token.
             
             account = FlairAccount(dev, refresh_token = dev.pluginProps['RefreshToken'])
@@ -222,16 +179,9 @@ class Plugin(indigo.PluginBase):
             
             dev.updateStateOnServer(key="authenticated", value=account.authenticated)
             self.update_needed = True
-                            
-        elif dev.deviceTypeId == 'FlairThermostat':
-
-            self.flair_thermostats[dev.id] = FlairThermostat(dev)
-            self.update_needed = True
-            
-            
+                                        
 
     def deviceStopComm(self, dev):
-
         self.logger.info(u"{}: Stopping {} Device {}".format( dev.name, dev.deviceTypeId, dev.id))
 
         if dev.deviceTypeId == 'FlairAccount':
@@ -255,60 +205,6 @@ class Plugin(indigo.PluginBase):
         else:
             return True  
 
-    ########################################
-    # Thermostat Action callbacks
-    ########################################
-    
-    # Main thermostat action bottleneck called by Indigo Server.
-   
-    def actionControlThermostat(self, action, dev):
-        self.logger.debug(u"{}: action.thermostatAction: {}".format(dev.name, action.thermostatAction))
-       ###### SET HVAC MODE ######
-        if action.thermostatAction == indigo.kThermostatAction.SetHvacMode:
-            self.handleChangeHvacModeAction(dev, action.actionMode)
-
-        ###### SET FAN MODE ######
-        elif action.thermostatAction == indigo.kThermostatAction.SetFanMode:
-            self.handleChangeFanModeAction(dev, action.actionMode, u"hvacFanIsOn")
-
-        ###### SET COOL SETPOINT ######
-        elif action.thermostatAction == indigo.kThermostatAction.SetCoolSetpoint:
-            newSetpoint = action.actionValue
-            self.handleChangeSetpointAction(dev, newSetpoint, u"setpointCool")
-
-        ###### SET HEAT SETPOINT ######
-        elif action.thermostatAction == indigo.kThermostatAction.SetHeatSetpoint:
-            newSetpoint = action.actionValue
-            self.handleChangeSetpointAction(dev, newSetpoint, u"setpointHeat")
-
-        ###### DECREASE/INCREASE COOL SETPOINT ######
-        elif action.thermostatAction == indigo.kThermostatAction.DecreaseCoolSetpoint:
-            newSetpoint = dev.coolSetpoint - action.actionValue
-            self.handleChangeSetpointAction(dev, newSetpoint, u"setpointCool")
-
-        elif action.thermostatAction == indigo.kThermostatAction.IncreaseCoolSetpoint:
-            newSetpoint = dev.coolSetpoint + action.actionValue
-            self.handleChangeSetpointAction(dev, newSetpoint, u"setpointCool")
-
-        ###### DECREASE/INCREASE HEAT SETPOINT ######
-        elif action.thermostatAction == indigo.kThermostatAction.DecreaseHeatSetpoint:
-            newSetpoint = dev.heatSetpoint - action.actionValue
-            self.handleChangeSetpointAction(dev, newSetpoint, u"setpointHeat")
-
-        elif action.thermostatAction == indigo.kThermostatAction.IncreaseHeatSetpoint:
-            newSetpoint = dev.heatSetpoint + action.actionValue
-            self.handleChangeSetpointAction(dev, newSetpoint, u"setpointHeat")
-
-        ###### REQUEST STATE UPDATES ######
-        elif action.thermostatAction in [indigo.kThermostatAction.RequestStatusAll, indigo.kThermostatAction.RequestMode,
-         indigo.kThermostatAction.RequestEquipmentState, indigo.kThermostatAction.RequestTemperatures, indigo.kThermostatAction.RequestHumidities,
-         indigo.kThermostatAction.RequestDeadbands, indigo.kThermostatAction.RequestSetpoints]:
-            self.update_needed = True
-
-        ###### UNTRAPPED CONDITIONS ######
-        # Explicitly show when nothing matches, indicates errors and unimplemented actions instead of quietly swallowing them
-        else:
-            self.logger.warning(u"{}: Unimplemented action.thermostatAction: {}".format(dev.name, action.thermostatAction))
 
     def actionControlUniversal(self, action, dev):
         self.logger.debug(u"{}: action.actionControlUniversal: {}".format(dev.name, action.deviceAction))
@@ -317,124 +213,10 @@ class Plugin(indigo.PluginBase):
         else:
             self.logger.warning(u"{}: Unimplemented action.deviceAction: {}".format(dev.name, action.deviceAction))
 
-
-    ########################################
-    # Activate Comfort Setting callback
-    ########################################
-    
-    def actionActivateComfortSetting(self, action, dev):
-        self.logger.debug(u"{}: actionActivateComfortSetting".format(dev.name))
-        defaultHold = dev.pluginProps.get("holdType", "nextTransition")
-
-        climate = action.props.get("climate")
-        holdType = action.props.get("holdType", defaultHold)
-        self.flair_thermostats[dev.id].set_climate_hold(climate, holdType)
-
-    def climateListGenerator(self, filter, valuesDict, typeId, targetId):                                                                                                                 
-        self.logger.debug(u"climateListGenerator: typeId = {}, targetId = {}".format(typeId, targetId))
-        return self.flair_thermostats[targetId].get_climates()
-
-    ########################################
-    # Set Hold Type
-    ########################################
-    
-    def actionSetDefaultHoldType(self, action, dev):
-        self.logger.debug(u"{}: actionSetDefaultHoldType".format(dev.name))
-         
-        props = dev.pluginProps
-        props["holdType"] = action.props.get("holdType", "nextTransition")
-        dev.replacePluginPropsOnServer(props)                
- 
- 
-    ########################################
-    # Resume Program callbacks
-    ########################################
-    
-    def menuResumeAllPrograms(self):
-        self.logger.debug(u"menuResumeAllPrograms")
-        for devId, thermostat in self.flair_thermostats.items():
-            if indigo.devices[devId].deviceTypeId == 'FlairThermostat':
-                thermostat.resume_program()
-
-    def menuResumeProgram(self, valuesDict, typeId):
-        self.logger.debug(u"menuResumeProgram")
-        try:
-            deviceId = int(valuesDict["targetDevice"])
-        except:
-            self.logger.error(u"Bad Device specified for Resume Program operation")
-            return False
-
-        for thermId, thermostat in self.flair_thermostats.items():
-            if thermId == deviceId:
-                thermostat.resume_program()
-        return True
         
-    def menuDumpThermostat(self):
-        self.logger.debug(u"menuDumpThermostat")
+    def menuDumpData(self):
+        self.logger.debug(u"menuDumpData")
         for accountID, account in self.flair_accounts.items():
             account.dump_data()
         return True
-
-    def actionResumeAllPrograms(self, action, dev):
-        self.logger.debug(u"actionResumeAllPrograms")
-        for devId, thermostat in self.flair_thermostats.items():
-            if indigo.devices[devId].deviceTypeId == 'FlairThermostat':
-                thermostat.resume_program()
-
-    def actionResumeProgram(self, action, dev):
-        self.logger.debug(u"{}: actionResumeProgram".format(dev.name))
-        self.flair_thermostats[dev.id].resume_program()
-    
-    def pickThermostat(self, filter=None, valuesDict=None, typeId=0):
-        retList = []
-        for dev in indigo.devices.iter("self"):
-            if dev.deviceTypeId == 'FlairThermostat':
-                retList.append((dev.id, dev.name))
-        retList.sort(key=lambda tup: tup[1])
-        return retList
-
-
-
-    ########################################
-    # Process action request from Indigo Server to change main thermostat's main mode.
-    ########################################
-
-    def handleChangeHvacModeAction(self, dev, newHvacMode):
-        hvac_mode = kHvacModeEnumToStrMap.get(newHvacMode, u"unknown")
-        self.logger.debug(u"{} ({}): Mode set to: {}".format(dev.name, dev.address, hvac_mode))
-
-        self.flair_thermostats[dev.id].set_hvac_mode(hvac_mode)
-        self.update_needed = True
-        if "hvacOperationMode" in dev.states:
-            dev.updateStateOnServer("hvacOperationMode", newHvacMode)
-
-    ########################################
-    # Process action request from Indigo Server to change a cool/heat setpoint.
-    ########################################
-    
-    def handleChangeSetpointAction(self, dev, newSetpoint, stateKey):
-
-        #   enforce minima/maxima based on the scale in use by the plugin
-        newSetpoint = self._constrainSetpoint(newSetpoint)
-
-        #   API uses F scale
-        newSetpoint = self._toFahrenheit(newSetpoint)
-
-        holdType = dev.pluginProps.get("holdType", "nextTransition")
-
-        if stateKey == u"setpointCool":
-            self.logger.info(u'{}: set cool to: {} and leave heat at: {}'.format(dev.name, newSetpoint, dev.heatSetpoint))
-            self.flair_thermostats[dev.id].set_hold_temp(newSetpoint, dev.heatSetpoint, holdType)
-
-        elif stateKey == u"setpointHeat":
-            self.logger.info(u'{}: set heat to: {} and leave cool at: {}'.format(dev.name, newSetpoint,dev.coolSetpoint))
-            self.flair_thermostats[dev.id].set_hold_temp(dev.coolSetpoint, newSetpoint, holdType)
-
-        else:
-            self.logger.error(u'{}: handleChangeSetpointAction Invalid operation - {}'.format(dev.name, stateKey))
-        
-        self.update_needed = True
-        if stateKey in dev.states:
-            dev.updateStateOnServer(stateKey, newSetpoint, uiValue="%.1f °F" % (newSetpoint))
-
 
